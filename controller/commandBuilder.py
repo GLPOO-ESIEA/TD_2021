b5d7a9a0e6bbd7309307d4e42ddaa08e1986b3d0
Thomas AUBIN <aubin@et.esiea.fr>
2021-03-05 23:35:58 +0100

    WIP command controller

---
+++ controller/commandBuilder.py
@@ -0,0 +1,25 @@
+
+
+class CommandBuilder:
+
+    def __init__(self, customer_id: str):
+        self._customer_id = customer_id
+        self._articles = list()
+
+    def add_article(self, article_id: str, number: int):
+        # TODO
+        # get article
+        article = None
+        # check stocks
+        return
+
+    def remove_article(self, article_id):
+        # TODO
+        return
+
+    def get_price(self):
+        return
+
+    def register_command(self):
+        # TODO
+        return True
