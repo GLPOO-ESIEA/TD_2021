88fc1d624095256ef8f5efcb0351a39e2f902645
Ulric_muad <sw.guillaume27@gmail.com>
2021-03-15 22:30:42 +0100

    Ajout lien dans main pour ressource

--- main_qt.py
+++ main_qt.py
@@ -4,6 +4,7 @@ from PySide6.QtWidgets import QApplication
 from vue.menu import MenuWindow
 
 #https://realpython.com/python-pyqt-layout/
+#https://www.learnpyqt.com/tutorials/creating-multiple-windows/
 
 def run():
     app = QApplication(sys.argv)
